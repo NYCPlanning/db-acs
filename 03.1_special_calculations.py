fix median calculation to use lower bound of top coding

03.1_special_calculations.py
@@ -25,7 +25,7 @@ def get_median(buckets, row):
     elif C == 0: 
         median =0
     elif i == len(buckets.keys())-1:
-        median = list(buckets.values())[-1][1]
+        median = list(buckets.values())[-1][0]
     else: 
         C = C - int(row[orderedE[i]])
         L = buckets[ordered[i]][0]
@@ -155,7 +155,7 @@ if __name__ == "__main__":
 
     # For the following special calculation, only apply to NTA
     df.loc[df.geotype=='NTA2010','percapince'] = df['agip15ple']/df['pop_6e']
-    df.loc[df.geotype=='NTA2010','percapincm'] = np.sqrt(df['agip15plm']**2 + (df['agip15ple']*df['pop_6m']/df['pop_6e'])**2)*df['pop_6e']
+    df.loc[df.geotype=='NTA2010','percapincm'] = 1/df['pop_6e']*np.sqrt(df['agip15plm']**2 + (df['agip15ple']*df['pop_6m']/df['pop_6e'])**2)
     df.loc[df.geotype=='NTA2010','percapincc'] = df.apply(lambda row: get_c(row['percapince'], row['percapincm']), axis=1)
     df['percapincz'] = np.nan
     df['percapincp'] = np.nan

03_calculation.py
@@ -16,7 +16,10 @@ def get_e_special(e):
 
 def get_m(m):
     result = sum(map(lambda x: x**2, filter(lambda x: ~np.isnan(x), m)))**0.5
-    return result
+    if result == 0: 
+        return np.nan
+    else: 
+        return result
 
 def calculate(category):
     df = pd.read_csv(f'data/{category}_intermediate.csv', index_col=False)

05_pivot.py
@@ -56,11 +56,21 @@ def pivot_output(category):
         r.append(dff)
 
     result = pd.concat(r)
-    result.loc[result.c < 0,'c'] = np.nan
+    result.loc[result.c <= 0,'c'] = np.nan
     result.loc[result.e < 0,'e'] = np.nan
-    result.loc[result.m < 0,'m'] = np.nan
+    result.loc[result.m <= 0,'m'] = np.nan
     result.loc[result.p < 0,'p'] = np.nan
     result.loc[result.z < 0,'z'] = np.nan
+
+    result.loc[result.e==0, 'c'] = np.nan
+    result.loc[result.e==0, 'm'] = np.nan
+    result.loc[result.e==0, 'p'] = np.nan
+    result.loc[result.e==0, 'z'] = np.nan
+
+    result.loc[result.m.isna(), 'c'] = np.nan
+    result.loc[result.m.isna(), 'p'] = np.nan
+    result.loc[result.m.isna(), 'z'] = np.nan
+
     result = result[['geotype', 'geogname', 'geoid', 'dataset', 'variable', 'c', 'e', 'm', 'p', 'z']]
     result.to_csv(f'data/{category}_final_pivoted.csv', index=False)
 